f4627259f498f5641a4157b8488d1dca49272cec
chung dev <chung@myovolt.com>
2022-01-13 09:24:01 +1300

    Setting up the Theme data

---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,25 @@
+import 'package:advanced_flutter/presentation/color_manager.dart';
+import 'package:flutter/material.dart';
+
+// Create function which will return Theme data
+ThemeData getApplicationTheme() {
+  return ThemeData(
+      //colors of the App
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1, //Will be use incase of disabled button
+      colorScheme:
+          ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey2)
+
+      //Card view theme
+
+      //App bar theme
+
+      //Button theme
+
+      //Text theme
+
+      //input decoration theme(text form field)
+      );
+}
