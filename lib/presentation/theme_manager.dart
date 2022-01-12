78699eb184c34f2492852ed034fd13f01f55f6d9
chung dev <chung@myovolt.com>
2022-01-13 09:43:57 +1300

    Added AppBarTheme values

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,6 @@
 import 'package:advanced_flutter/presentation/color_manager.dart';
+import 'package:advanced_flutter/presentation/font_manager.dart';
+import 'package:advanced_flutter/presentation/styles_manager.dart';
 import 'package:advanced_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +11,10 @@ ThemeData getApplicationTheme() {
     primaryColor: ColorManager.primary,
     primaryColorLight: ColorManager.primaryOpacity70,
     primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1, //Will be use incase of disabled button
+    disabledColor: ColorManager.grey1,
+    //ripple color
+    splashColor: ColorManager.primaryOpacity70,
+    // //Will be use incase of disabled button
     colorScheme:
         ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey2),
 
@@ -19,8 +24,17 @@ ThemeData getApplicationTheme() {
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4,
     ),
-    //App bar theme
 
+    //App bar theme
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s16,
+        )),
     //Button theme
 
     //Text theme
