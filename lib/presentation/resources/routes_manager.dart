7fe0bb3723c2089a682c928f81c82d32decdd76d
chung dev <chung@myovolt.com>
2022-01-13 15:02:08 +1300

    Added Route Manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,12 @@
+import 'package:advanced_flutter/presentation/forgot_password/forgot_password.dart';
+import 'package:advanced_flutter/presentation/login/login.dart';
+import 'package:advanced_flutter/presentation/main/main_view.dart';
+import 'package:advanced_flutter/presentation/on_boarding/on_boarding.dart';
+import 'package:advanced_flutter/presentation/register/register.dart';
+import 'package:advanced_flutter/presentation/splash/splash.dart';
+import 'package:advanced_flutter/presentation/store_details/store_details.dart';
+import 'package:flutter/material.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String onBoardingRoute = "/onBoarding";
@@ -6,4 +15,39 @@ class Routes {
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
   static const String forgotPasswordRoute = "/forgotPassword";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text('Route not found'),
+              ),
+              body: const Center(
+                child: Text('Page not found'),
+              ),
+            ));
+  }
 }
