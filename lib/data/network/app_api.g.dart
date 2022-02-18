e6ad3988d7c0dd5281de2567391e8f75270c61d6
chung dev <chung@myovolt.com>
2022-02-19 11:21:40 +1300

    Create App API code generate

---
+++ lib/data/network/app_api.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_api.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+class _AppServiceClient implements AppServiceClient {
+  _AppServiceClient(this._dio, {this.baseUrl}) {
+    baseUrl ??= 'http://chungle.mocklab.io';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
